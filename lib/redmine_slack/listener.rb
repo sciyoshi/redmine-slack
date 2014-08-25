79f917f742fcf2564df1a16c8e117efb7685669c
Samuel Cormier-Iijima <sam@fluidware.com>
2014-08-25 10:30:49 -0400

    Support multiple Slack integrations for different projects.
    The Slack integration can now be customized on a per-project basis by creating
    a custom Project field named "Slack URL".
    
    Fixes #17.

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -5,8 +5,9 @@ class SlackListener < Redmine::Hook::Listener
 		issue = context[:issue]
 
 		channel = channel_for_project issue.project
+		url = url_for_project issue.project
 
-		return unless channel
+		return unless channel and url
 
 		msg = "[#{escape issue.project}] #{escape issue.author} created <#{object_url issue}|#{escape issue}>"
 
@@ -26,7 +27,7 @@ class SlackListener < Redmine::Hook::Listener
 			:short => true
 		}]
 
-		speak msg, channel, attachment
+		speak msg, channel, attachment, url
 	end
 
 	def controller_issues_edit_after_save(context={})
@@ -34,8 +35,9 @@ class SlackListener < Redmine::Hook::Listener
 		journal = context[:journal]
 
 		channel = channel_for_project issue.project
+		url = url_for_project issue.project
 
-		return unless channel
+		return unless channel and url
 
 		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>"
 
@@ -43,11 +45,11 @@ class SlackListener < Redmine::Hook::Listener
 		attachment[:text] = escape journal.notes if journal.notes
 		attachment[:fields] = journal.details.map { |d| detail_to_field d }
 
-		speak msg, channel, attachment
+		speak msg, channel, attachment, url
 	end
 
-	def speak(msg, channel, attachment=nil)
-		url = Setting.plugin_redmine_slack[:slack_url]
+	def speak(msg, channel, attachment=nil, url=nil)
+		url = Setting.plugin_redmine_slack[:slack_url] if not url
 		username = Setting.plugin_redmine_slack[:username]
 		icon = Setting.plugin_redmine_slack[:icon]
 
@@ -80,6 +82,18 @@ private
 
 	def object_url(obj)
 		Rails.application.routes.url_for(obj.event_url({:host => Setting.host_name, :protocol => Setting.protocol}))
+	end
+
+	def url_for_project(proj)
+		return nil if proj.blank?
+
+		cf = ProjectCustomField.find_by_name("Slack URL")
+
+		return [
+			(proj.custom_value_for(cf).value rescue nil),
+			(url_for_project proj.parent),
+			Setting.plugin_redmine_slack[:slack_url],
+		].find{|v| v.present?}
 	end
 
 	def channel_for_project(proj)
