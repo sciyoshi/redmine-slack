68366c847cacfdc3f88f59f34bc1dc347912a1d9
Michel Antoine <antoin_m@epitech.eu>
2015-02-25 16:20:30 +0100

    Add responsables

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -33,6 +33,11 @@ class SlackListener < Redmine::Hook::Listener
 			:short => true
 		} if Setting.plugin_redmine_slack[:display_watchers] == 'yes'
 
+		attachment[:fields] << {
+			:title => I18n.t("default_role_manager"),
+			:value => escape(User.find issue.custom_field_value(CustomField.where(name: 'Responsable').first)),
+			:short => true
+		}
 		speak msg, channel, attachment, url
 	end
 
@@ -154,7 +159,7 @@ private
 		when "category"
 			category = IssueCategory.find(detail.value) rescue nil
 			value = escape category.to_s
-		when "assigned_to"
+		when "assigned_to", 'Responsable'
 			user = User.find(detail.value) rescue nil
 			value = escape user.to_s
 		when "fixed_version"
