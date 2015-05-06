9b07151093b365358c359295b1280b38083edf0a
Stephen Yeargin <stephen.yeargin@gmail.com>
2015-05-06 16:42:42 -0500

    Make issue update posting optional
    This gets noisy really fast with a team of more than two people. Adds a setting to allow the desired channel to announce only new tickets.

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -37,7 +37,7 @@ class SlackListener < Redmine::Hook::Listener
 		channel = channel_for_project issue.project
 		url = url_for_project issue.project
 
-		return
+		return unless channel and url and Setting.plugin_redmine_slack[:post_updates] == '1'
 
 		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>"
 
