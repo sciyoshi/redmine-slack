a59bcef0cae0e8490e20280eb67f1d9c1fa6df48
Ben Fitzhardinge <ben.fitzhardinge@ch2.net.au>
2016-06-23 11:22:17 +1000

    Option for posting completed issue updates only
    Signed-off-by: Ben Fitzhardinge <ben.fitzhardinge@ch2.net.au>

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -44,7 +44,7 @@ class SlackListener < Redmine::Hook::Listener
 		channel = channel_for_project issue.project
 		url = url_for_project issue.project
 
-		return unless channel and url and Setting.plugin_redmine_slack[:post_updates] == '1'
+		return unless channel and url and (Setting.plugin_redmine_slack[:post_updates] == '1' or ( Setting.plugin_redmine_slack[:post_closed] == '1' and issue.status.is_closed ) )
 		return if issue.is_private?
 
 		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>#{mentions journal.notes}"
